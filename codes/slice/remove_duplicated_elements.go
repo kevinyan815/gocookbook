2ae2ea2a2eb318b0ecc9dcf6c569d1b81cfab9f1
Kevin Yan <kevinyan815@gmail.com>
2019-08-06 11:40:02 +0800

    Update remove_duplicated_elements.go

--- codes/slice/remove_duplicated_elements.go
+++ codes/slice/remove_duplicated_elements.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+    "fmt"
+)
+
+func main() {
+    s := []string{"hello", "world", "hello", "golang", "hello", "ruby", "php", "java"}
+
+    fmt.Println(removeDuplicateElement(s)) //output: hello world golang ruby php java
+}
+
+func removeDuplicateElement(languages []string) []string {
+    result := make([]string, 0, len(languages))
+    temp := map[string]struct{}{}
+    for _, item := range languages {
+        if _, ok := temp[item]; !ok {
+            temp[item] = struct{}{}
+            result = append(result, item)
+        }
+    }
+    return result
+}
+
+// output: ["hello", "world", "golang", "ruby", "php", "java"]
