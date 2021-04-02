101735f4209b51a70edc28498251d8a6142df895
Kevin Yan <kevinyan815@gmail.com>
2021-04-02 10:42:40 +0800

    Update order_num.go

--- codes/tag_picker/order_num.go
+++ codes/tag_picker/order_num.go
@@ -6,10 +6,16 @@ type OrderNumTagPicker struct {
 	ValueCh chan interface{}
 }
 
+type TradeNoInfo struct {
+	TradeNo string `json:"trade_no"`
+}
+
 func (picker *OrderNumTagPicker) PickTagValueForUser(userId int64, args ...interface{}) {
 	// 用类型转换得到交易号
-	tradeNo, ok := args[0].(string)
-	if !ok {
+       	// tradeNo, ok := args[0].(string)
+        extInfoJson, _ := args[0].(string)
+
+	if err := json.Unmarshal([]byte(extInfoJson), &TradeNoInfo); err != nil {
                 // log.Error自己实现 
 		log.Error("PayTotalTagPickerError", "Invalid arg", args[0])
 		// 结束执行并通知外部
@@ -19,8 +25,7 @@ func (picker *OrderNumTagPicker) PickTagValueForUser(userId int64, args ...inter
         // 这里就打印下参数值，标签查询的具体逻辑自己实现
         fmt.Println(userId)
         fmt.Println(TradeNo)
-        ......
-	// 查询到的用户的标签
+	// 查询到的用户的标签 (假设交易号下有10个订单）
 	picker.ValueCh <- 10
 }
 
