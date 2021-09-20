16c4af43324c28c30bf65cad07054b3333180b70
Kevin Yan <kevinyan815@gmail.com>
2021-09-20 15:22:44 +0800

    Update http_util.go

--- codes/httptool/http_util.go
+++ codes/httptool/http_util.go
@@ -1,4 +1,4 @@
-package main
+package httputil
 
 import (
 	"io/ioutil"
@@ -8,65 +8,104 @@ import (
 	"time"
 )
 
-
-
-func httpRequest(method string, url string, data string, headers map[string]string, timeout time.Duration) (code int, content string, err error) {
+func httpRequest(method string, url string, options ...*Option) (code int, content string, err error) {
 	start := time.Now()
+
+	reqOpts := defaultRequestOptions() // 默认的请求选项
+	for _, opt := range options {      // 在reqOpts上应用通过options设置的选项
+		opt.apply(reqOpts)
+	}
+	// 创建请求对象
+	req, err := http.NewRequest(method, url, strings.NewReader(reqOpts.data))
+	// 记录请求日志
 	defer func() {
-		dur := int64(time.Since(start) / time.Millisecond)
+		dur := int64(time.Since(start) / time.Second)
 		if dur >= 500 {
-			log.Println("HttpPost", url, "in", data, "out", content, "err", err, "dur/ms", dur)
+			log.Println("Http"+method, url, "in", reqOpts.data, "out", content, "err", err, "dur/ms", dur)
 		} else {
-			log.Println("HttpPost", url, "in", data, "out", content, "err", err, "dur/ms", dur)
+			log.Println("Http"+method, url, "in", reqOpts.data, "out", content, "err", err, "dur/ms", dur)
 		}
 	}()
-	
-	req, err := http.NewRequest(method, url, strings.NewReader(data))
-	if len(headers) != 0 {
-		for key, value := range headers {
+	defer req.Body.Close()
+
+	if len(reqOpts.headers) != 0 { // 设置请求头
+		for key, value := range reqOpts.headers {
 			req.Header.Add(key, value)
 		}
 	}
 	if err != nil {
 		return
 	}
-	defer req.Body.Close()
-
-	client := &http.Client{Timeout: timeout}
+	// 发起请求
+	client := &http.Client{Timeout: reqOpts.timeout}
 	resp, error := client.Do(req)
 	if error != nil {
 		return 0, "", error
 	}
-
+	// 解析响应
 	defer resp.Body.Close()
 	code = resp.StatusCode
 	result, _ := ioutil.ReadAll(resp.Body)
 	content = string(result)
-	
+
 	return
 }
 
 // 发起GET请求
-func HttpGet(url string, data string, timeout time.Duration) (code int, content string, err error) {
-	return httpRequest("GET", url, data, nil, timeout )
-}
-
-func HttpGetWithHeaders(url string, data string, headers map[string]string, timeout time.Duration) (code int, content string, err error) {
-	return httpRequest("GET", url, data, headers, timeout )
+func HttpGet(url string, options ...*Option) (code int, content string, err error) {
+	return httpRequest("GET", url, options...)
 }
 
 // 发起POST请求，请求头指定Content-Type: application/json
 func HttpJsonPost(url string, data string, timeout time.Duration) (code int, content string, err error) {
-	headers := map[string]string {
+	headers := map[string]string{
 		"Content-Type": "application/json",
 	}
-	code, content, err = httpRequest("POST", url, data, headers, timeout)
+	code, content, err = httpRequest(
+		"POST", url, WithTimeout(timeout), WithHeaders(headers), WithData(data))
+
 	return
 }
 
-// 发起POST请求，请求头由调用方指定
-func HttpPostWithHeaders(url string, data string, headers map[string]string, timeout time.Duration) (code int, content string, err error) {
-	code, content, err = httpRequest("POST", url, data, headers, timeout)
-	return
+// 针对可选的HTTP请求配置项，模仿gRPC使用的Options设计模式实现
+type requestOption struct {
+	timeout time.Duration
+	data    string
+	headers map[string]string
 }
 
+type Option struct {
+	apply func(option *requestOption)
+}
+
+func defaultRequestOptions() *requestOption {
+	return &requestOption{ // 默认请求选项
+		timeout: 5 * time.Second,
+		data:    "",
+		headers: nil,
+	}
+}
+
+func WithTimeout(timeout time.Duration) *Option {
+	return &Option{
+		apply: func(option *requestOption) {
+			option.timeout = timeout
+		},
+	}
+}
+
+func WithHeaders(headers map[string]string) *Option {
+	return &Option{
+		apply: func(option *requestOption) {
+			option.headers = headers
+		},
+	}
+}
+
+func WithData(data string) *Option {
+	return &Option{
+		apply: func(option *requestOption) {
+			option.data = data
+		},
+	}
+}
