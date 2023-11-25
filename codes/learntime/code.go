8055fbfaced18c9c1e74139b8890d5f3523af748
Kevin Yan <kevinyan815@gmail.com>
2023-11-25 14:38:52 +0800

    add parse time in location codes and time add function

--- codes/learntime/code.go
+++ codes/learntime/code.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	timeStr := "2023-10-11 23:34:45"
-	comparedTime, _ := parseFromString(timeStr, TimeLayoutDefault)
-	fmt.Printf("%s晚于当前时间: %v", timeStr, isAfterNow(comparedTime))
+	comparedTime, _ := parseInLocalLocation(timeStr, TimeLayoutDefault)
+	fmt.Printf("%s晚于当前时间: %v\n", timeStr, isAfterNow(comparedTime))
+	originTime := comparedTime
+	aWeekLaterTime := weekLater(originTime, 1)
+	fmt.Printf("初始时间: %v, 一周后的时间: %v\n", originTime, aWeekLaterTime)
 }
 
 const TimeLayoutDefault = "2006-01-02 15:04:05"
@@ -16,8 +19,22 @@ const TimeLayoutDate = "20060102"
 const TimeLayoutDateWithHyphen = "2006-01-02"
 
 
+// parseFromString 解析字符串时间
 func parseFromString(timeStr, layout string) (timeObj time.Time, err error) {
 	timeObj, err = time.Parse(layout, timeStr)
+	return
+}
+
+// parseInLocalLocation 用本地时区解析字符串时间
+func parseInLocalLocation(timeStr, layout string) (timeObj time.Time, err error) {
+	timeObj, err = time.ParseInLocation(layout, timeStr, time.Local)
+	return
+}
+
+// parseInSpainLocation 用西班牙时区解析字符串时间
+func parseInSpainLocation(timeStr, layout string) (timeObj time.Time, err error) {
+	loc, _ := time.LoadLocation("Europe/Madrid")
+	timeObj, err = time.ParseInLocation(layout, timeStr, loc)
 	return
 }
 
@@ -27,4 +44,8 @@ func isBeforeNow(comparedTime time.Time) bool {
 
 func isAfterNow(comparedTime time.Time) bool {
 	return comparedTime.After(time.Now())
+}
+
+func weekLater(originTime time.Time, w int) time.Time {
+	return originTime.AddDate(0, 0, w * 7)
 }
