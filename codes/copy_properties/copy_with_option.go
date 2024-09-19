8bf25d00ac301f27fbfdb75c17f506655c17bd01
Kevin Yan <kevinyan815@gmail.com>
2024-09-19 18:27:31 +0800

    Update copy_with_option.go

--- codes/copy_properties/copy_with_option.go
+++ codes/copy_properties/copy_with_option.go
@@ -22,7 +22,26 @@ func CopyProperties(dst, src interface{}) error {
 					}
 					return s.Format("2006-01-02 15:04:05"), nil
 				},
-			}
+			},
+			{ // 字符串转成time.Time
+				SrcType: copier.String,
+				DstType: time.Time{},
+				Fn: func(src interface{}) (dst interface{}, err error) {
+					s, ok := src.(string)
+
+					if !ok {
+						return nil, errors.New("src type is not time format string")
+					}
+					pattern := `^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$` // YYYY-MM-DD HH:MM:SS
+					matched, _ := regexp.MatchString(pattern, s)
+					if matched {
+						return time.Parse("2006-01-02 15:04:05", s)
+					}
+					return nil, errors.New("src type is not time format string")
+				},
+			},
+		},
+		
 	})
 
 	return err
